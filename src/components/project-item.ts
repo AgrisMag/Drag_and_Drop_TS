8bc9139b31f20f9fe8f5009f76dea22e4bb0a703
AgrisMagdalenoks <agris.magdalenoks@28stone.com>
2022-04-25 15:49:17 +0300

    Adding namespaces and new directories

--- src/components/project-item.ts
+++ src/components/project-item.ts
@@ -1,4 +1,7 @@
 /// <reference path="base-component.ts" />
+/// <reference path="../decorators/autobind.ts" />
+/// <reference path="../models/drag-drop-interfaces.ts" />
+/// <reference path="../models/project-model.ts" />
 
 namespace App {
   // Project Item Class
